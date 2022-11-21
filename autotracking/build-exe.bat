a076c07c9a961d36b886fb7354f114fc039581f8
kbranch <kbranch@kbranch.net>
2022-11-20 21:58:27 -0500

    Updated the Windows build scripts

--- autotracking/build-exe.bat
+++ autotracking/build-exe.bat
@@ -1,1 +1,5 @@
+rd /S /Q build
+del magpie-autotracker.spec
+del dist\magpie-local.zip
+
 pyinstaller --noconfirm --onefile -n magpie-autotracker --console "autotracker.py"
--- buildLocal.bat
+++ buildLocal.bat
@@ -1,5 +1,12 @@
+rd /S /Q dist\magpie-data
+rd /S /Q build
+del magpie-data.spec
+del dist\magpie-local.zip
+
 pyinstaller --noconfirm --onedir --windowed -n magpie-data --add-data "LADXR;LADXR/" --add-data "templates;templates/" --add-data "static;static/"  "magpie.py"
+
 copy magpie.bat dist\
 cd dist
+
 tar.exe -a -c -f magpie-local.zip magpie-data magpie.bat
 cd ..
